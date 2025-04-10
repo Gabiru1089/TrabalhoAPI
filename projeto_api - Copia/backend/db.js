6b9014e6fa9f1e7d30af1afefeab66024c866549
Gabiru1089 <130368772+Gabiru1089@users.noreply.github.com>
2025-04-10 17:02:53 -0300

    Delete projeto_api - Copia directory

--- projeto_api - Copia/backend/db.js
+++
@@ -1,9 +0,0 @@
-import mysl from "mysql";
-
-
-export const db = mysl.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Papelhigienico1!",
-    database: "projeto_api"
-});
